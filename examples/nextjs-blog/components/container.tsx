4e81d9fbd43e18e45a64ae604aad43c180d4f8da
Adi Aswara <a.aswara@gmail.com>
2021-12-29 15:46:06 +0700

    Dev 112 examples blog redesign(margin logo)

--- examples/nextjs-blog/components/container.tsx
+++ examples/nextjs-blog/components/container.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 const Container: FunctionComponent = ({ children }: Props) => {
-  return <div className="container mx-auto px-2 md:px-6">{children}</div>;
+  return <div className="container mx-auto px-2 md:px-16">{children}</div>;
 };
 
 export default Container;
--- examples/nextjs-blog/components/header.tsx
+++ examples/nextjs-blog/components/header.tsx
@@ -3,13 +3,16 @@ import Container from './container';
 
 const Header = () => {
   return (
-    <nav className="mb-6 sm:mb-20 py-4 border-b border-gray-200 sticky top-0 bg-white">
+    <nav className="mb-6 sm:mb-20 pb-4 pt-3 border-b border-gray-200 sticky top-0 bg-white">
       <Container>
         <Link href="/">
           <img
             className="h-8 cursor-pointer sm:h-10"
             alt="Logo"
             src="/logo.svg"
+            style={{
+              marginLeft: -4
+            }}
           />
         </Link>
       </Container>
