c260696e3778e0cd259cfb70365461ef291e9cd9
Egi Ginting <exneval.rayz@gmail.com>
2022-04-05 16:54:17 +0700

    fix(sdk/js): follow up e083ac0
    Signed-off-by: Egi Ginting <exneval.rayz@gmail.com>

--- packages/sdk/js/src/realtime/RealtimeClient.ts
+++ packages/sdk/js/src/realtime/RealtimeClient.ts
@@ -62,8 +62,8 @@ export default class RealtimeClient {
           function (message: any) {
             callback?.({
               key,
-              event: message.result.data.data.eventType,
-              payload: message.result.data.data.payload,
+              event: message.data.eventType,
+              payload: message.data.payload,
             });
           },
         );
